12bd0dd4b5fbf8038d962259408fa51bf61a5ab7
salman <mrsalmanxzs@gmail.com>
2026-01-17 10:15:49 +0530

    lines changed

--- Python/136.py
+++ Python/136.py
@@ -33,7 +33,7 @@
 
 
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
+    def singleNumber(self, nums) -> int:
         result = 0 
         for num in nums:
             result ^= num
