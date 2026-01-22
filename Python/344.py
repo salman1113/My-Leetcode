c17ef53b6f45ad8d55f8335ed185446ce917f5d2
salman <mrsalmanxzs@gmail.com>
2026-01-22 17:31:44 +0530

    New problem

---
+++ Python/344.py
@@ -0,0 +1,32 @@
+# 344. Reverse String
+# Easy
+# Topics
+# premium lock icon
+# Companies
+# Hint
+# Write a function that reverses a string. The input string is given as an array of characters s.
+
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+ 
+
+# Example 1:
+
+# Input: s = ["h","e","l","l","o"]
+# Output: ["o","l","l","e","h"]
+# Example 2:
+
+# Input: s = ["H","a","n","n","a","h"]
+# Output: ["h","a","n","n","a","H"]
+
+
+# answer
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+       left, right = 0, len(s) - 1
+
+       while left < right:
+        s[left], s[right] = s[right], s[left]
+        left += 1
+        right -= 1
